e00c6665988080e8ba6536697449411f08f0edb9
bokhary <e.hussein@myfatoorah.com>
2019-05-15 12:21:01 +0300

    Initial Commit

---
+++ MFSDKCocoapod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MFSDKCocoapod
+//
+//  Created by Elsayed Hussein on 5/15/19.
+//  Copyright © 2019 Elsayed Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
